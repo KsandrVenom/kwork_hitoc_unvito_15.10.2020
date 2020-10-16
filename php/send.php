c73caa7c558f9b226f679a905de2f893e2eded0f
KsandrVenom <clif007@yandex.ru>
2020-10-16 16:46:01 +0300

    Update send.php

--- php/send.php
+++ php/send.php
@@ -1,26 +1,8 @@
 <?php
 
 $name = $_POST['name'];
-$tel = $_PoST['tel'];
-$email = $_POST['email'];
-
-$name = htmlspecialchars($name);
-$tel = htmlspecialchars($tel);
-$email = htmlspecialchars($email);
-
-$name = urldecode($name);
-$tel = urldecode($tel);
-$email = urldecode($email);
-
-$name = trim($name);
-$tel = trim($tel);
-$email = trim($email);
-
 echo $name;
-echo "<br>";
-echo $tel;
-echo "<br>";
-echo $email;
+
 
 ?>
 
